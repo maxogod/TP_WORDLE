f2ef25704ffd628acfd2a37163260e00c90111d2
maxogod <maxi18wars@gmail.com>
2022-05-14 17:42:09 -0300

    tt

--- utiles.py
+++ utiles.py
@@ -6,7 +6,7 @@ def obtener_color(color):
         "Defecto": "\x1b[39m"
     }
     return colores[color]
-
+#AAb
 
 def obtener_palabras_validas():
     return ["abran", "abria", "acojo", "actuo", "aguda", "agudo", "algas", \
