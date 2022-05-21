69848b592cda8111e7eec586e535404d6a4997a4
Florencia Russo <flopyrusso@hotmail.com>
2022-05-21 03:52:42 -0300

    Update TP1_UNO.py

--- TP1_UNO.py
+++ TP1_UNO.py
@@ -3,7 +3,9 @@ import utiles
 
 def validar_aciertos(palabra_a_adivinar, palabra_arriesgada):
     '''
-    Recibe la palabra a adivinar y la palabra ingresada por el usuario, ambas en mayusculas. Retorna la palabra arriesgada, cada letra con su color correspondiente segun si esta en la palabra a adivinar y en la posicion adecuada (verde), si aparece en la palabra a adivinar pero en otra posicion (amarillo) o si no esta en la palabra (gris oscuro). Cada letra esta separada por espacios para mejor lectura
+    Recibe la palabra a adivinar y la palabra ingresada por el usuario, ambas en mayusculas. 
+    Retorna la palabra arriesgada, cada letra con su color correspondiente segun si esta en la palabra a adivinar y en la posicion adecuada (verde), si aparece en la palabra a adivinar pero en otra posicion (amarillo) o si no esta en la palabra (gris oscuro). 
+    Cada letra esta separada por espacios para mejor lectura.
     '''
 
     palabra_pintada = ""
