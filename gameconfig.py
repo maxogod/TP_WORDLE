cfc073daba518440a228d03329b949eec57d3e97
Maximo Utrera <105026197+maxogod@users.noreply.github.com>
2022-07-02 14:34:47 -0300

    Create gameconfig.py

---
+++ gameconfig.py
@@ -0,0 +1,59 @@
+
+def read_config_file(config):
+    # Esta funcion puede ser reemplazada si ya hay una que lee archivos,
+    # Pero atencion con el return de los dos vacios
+    line = config.readline()
+    return line.rstrip("\n").split(",") if line else ['','']
+
+
+def config_dic_mod(config_file, config_dic):
+    """
+    Modifica y devuelve el diccionario config_dic que obtiene,
+    en base a el archivo config_file que recibe abierto, sino puede
+    no modificara cierta configuracion.
+    """
+    min_long_palabra = 3
+    max_long_palabra = 15
+    min_max_partida = 2
+    max_max_partida = 10
+    try:
+        config, value = read_config_file(config_file)
+    except ValueError:
+        config, value = ['', '']
+
+    while config:
+        if config == 'LONGITUD_PALABRA_SECRETA':
+            if min_long_palabra <= int(value) <= max_long_palabra:
+                config_dic['LONGITUD_PALABRA_SECRETA'] = [int(value), 'por config.']
+
+        elif config == 'MAXIMO_PARTIDAS':
+            if min_max_partida <= int(value) <= max_max_partida:
+                config_dic['MAXIMO_PARTIDAS'] = [int(value), 'por config.']
+
+        elif config == 'REINICIAR_ARCHIV0_PARTIDAS':
+            if value == 'False':
+                config_dic['REINICIAR_ARCHIV0_PARTIDAS'] = [False, 'por config.']
+            elif value == 'True':
+                config_dic['REINICIAR_ARCHIV0_PARTIDAS'] = [True, 'por config.']
+
+        try:
+            config, value = read_config_file(config_file)
+        except ValueError:
+            config, value = ['', '']
+
+    return config_dic
+
+def config_main():
+    """
+    retorna lo que obtenga de config_dic_mod, en caso que no se pueda
+    acceder al archivo config_file.csv retorna un dic de config por default.
+    """
+    config_default = {'LONGITUD_PALABRA_SECRETA': [5, 'por default.'],
+                  'MAXIMO_PARTIDAS': [5, 'por default.'],
+                  'REINICIAR_ARCHIV0_PARTIDAS': [False, 'por default.']}
+    try:
+        with open("config_file.csv") as config:
+            config_leida = config_dic_mod(config, config_default)
+            return config_leida
+    except FileNotFoundError:
+        return config_default
