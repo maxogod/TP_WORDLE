e03b5b461021e57610ebb4a7967c483e972be435
maxogod <maxi18wars@gmail.com>
2022-07-04 12:30:51 -0300

    arreglos visuales, y agg de funcion de msg para registro, para organizar mejor

--- login_signup.py
+++ login_signup.py
@@ -44,7 +44,6 @@ def emergentwindow(username, password):
     si se valido el usuario y contraseña llama a funcion successwindow sino a errorwindow.
     #Maximo Utrera
     """
-    #TODO change window messages accordingly
     if validacion_usuario_para_login(username, password):
         Jugadores(username)
         successwindow()
@@ -57,7 +56,7 @@ def successwindow():
     muestra mensaje de logueado correctamente en una ventana.
     #Maximo Utrera
     """
-    messagebox.showinfo('Success', 'Logueado correctamente')
+    messagebox.showinfo('Success', 'Logueado correctamente, ahora jugador 2 ingrese sus datos.')
 
 
 def errorwindow():
@@ -65,8 +64,7 @@ def errorwindow():
     muestra mensaje de error al loguearse en una ventana.
     #Maximo Utrera
     """
-    messagebox.showerror('Error', 'Error, No pudo loguearse, o no esta registrado '
-                                  'o ya esta logueado')
+    messagebox.showerror('Error', 'Error, El usuario o la contraceña no son correctos.')
 
 def aplicar_login(username, password, usernameEntry, passwordEntry):
     '''
@@ -79,40 +77,64 @@ def aplicar_login(username, password, usernameEntry, passwordEntry):
 
 def aplicar_register(username, password, password2, rootRegistro, nombre_archivo):
     '''
-    Se la llama en el boton de aceptar de la ventana de registro. Llama a la funcion registrar_nuevo_usuario y cierra la ventana de registro al finalizar.
+    Se la llama en el boton de enter de la ventana de registro.
+    Llama a la funcion registrar_nuevo_usuario y cierra la ventana de registro al finalizar.
     Florencia Russo
     '''
     registrar_nuevo_usuario(username, password, password2, nombre_archivo)
     rootRegistro.destroy()
 
+def emergent_windows_registro(tipo_msg, username, mensaje):
+    """
+    muestra los mendajes correspondientes segun los parametros.
+    #Maximo Utrera
+    """
+    posibles_msg = {'username invalido' : f'Nombre de usuario {username} invalido. '
+                                          'Debe contener letras, numeros y _ unicamente',
+                    'claves diferentes' : 'Ambas claves deben ser iguales',
+                    'clave invalida' : 'Clave invalida. '
+                                       'La clave debe tener una longitud de entre 8 y 15 caracteres, '
+                                       'y estar formada por al menos una mayuscula, una minuscula y _ o -',
+                    'usuario creado' : f'Usuario {username} fue creado.',
+                    'ya registrado': f'Usuario {username} ya registrado'}
+    if tipo_msg == 'error':
+        messagebox.showerror('error', posibles_msg[mensaje])
+    elif tipo_msg == 'info':
+        messagebox.showinfo('success', posibles_msg[mensaje])
+    elif tipo_msg == 'warning':
+        messagebox.showwarning('warning', posibles_msg[mensaje])
+
 def registrar_nuevo_usuario(usuario, clave, clave2, nombre_archivo):
     '''
-    Guarda un nuevo usuario en el archivo de usuarios.csv si no existe y si no genera mensaje de usuario existente 
+    Guarda un nuevo usuario en el archivo de usuarios.csv
+    si no existe y si no genera mensaje de usuario existente
     Florencia Russo
     '''
-    if(not validar_nombre_usuario(usuario)):
-        messagebox.showerror('', f'Nombre de usuario {usuario} invalido. Debe contener letras, numeros y _ unicamente')
-    elif(clave != clave2):
-        messagebox.showerror('', f'Ambas claves deben ser iguales')
-    elif(not validar_clave(clave)):
-       messagebox.showerror('', f'Clave invalida. La clave debe tener una longitud de entre 8 y 15 caracteres, y estar formada por al menos una mayuscula, una minuscula y _ o -')
-    elif (not validacion_usuario_para_login(usuario, clave)):
+    if not validar_nombre_usuario(usuario):
+        emergent_windows_registro('error', usuario, 'username invalido')
+    elif clave != clave2:
+        emergent_windows_registro('error', usuario, 'claves diferentes')
+    elif not validar_clave(clave):
+       emergent_windows_registro('error', usuario, 'clave invalida')
+    elif not validacion_usuario_para_login(usuario, clave):
         with open(nombre_archivo, 'a', encoding = "utf8") as archivo:
             archivo.write(f'{usuario},{clave}\n')
-        messagebox.showinfo("", f'Usuario {usuario} creado')
+        emergent_windows_registro('info', usuario, 'usuario creado')
     else: 
-        messagebox.showwarning('', f'Usuario {usuario} ya registrado')
+        emergent_windows_registro('warning', usuario, 'ya registrado')
 
 def validar_nombre_usuario(nombre_usuario):
     '''
-    Valida que el nombre de usuario ingresado tenga la longitud deseada y este formado solo por letras, numeros y _ o -.
+    Valida que el nombre de usuario ingresado tenga la longitud deseada y este formado solo por letras,
+    numeros y _ o -.
     Florencia Russo
     '''
     nombre_valido = False
     largo_min = 4
     largo_max = 15
     nombre_letras = nombre_usuario.replace('_', '')
-    if ((largo_min <= len(nombre_usuario) <= largo_max) and nombre_letras.isalnum() and '_' in nombre_usuario):
+    if ((largo_min <= len(nombre_usuario) <= largo_max) and
+            nombre_letras.isalnum() and '_' in nombre_usuario):
         nombre_valido = True
     
     return nombre_valido
@@ -156,16 +178,24 @@ def validar_clave(clave):
 def gui_registro():
     '''
     Crea la pantalla con el formulario de registro.
-    Florencia Russo
+    #Florencia Russo
+    el "look" de la interfaz.
+    #Maximo Utrera
     '''
-    #TODO make gui look better
+
     rootRegistro = Toplevel()
-    rootRegistro.title("Wordle G1 - Registrar nuevo usuario")
+    rootRegistro.iconbitmap('archivos/pikachu.ico')
+    rootRegistro.title("Wordle G1 | Sign-up")
     rootRegistro.resizable(0,0)
-    rootRegistro.geometry("350x350")
+    rootRegistro.geometry("350x150")
+    rootRegistro.config(bg='sky blue')
 
     frameRegistro = Frame(rootRegistro)
     frameRegistro.pack()
+    frameRegistro.config(bg='black')
+    frameRegistro.config(relief='groove')
+    frameRegistro.config(bd=5)
+    frameRegistro.grid(rowspan=5, columnspan=5, padx=50, pady=20)
 
     usuarioRegistro = StringVar()
     contrasenaRegistro = StringVar()
@@ -173,23 +203,40 @@ def gui_registro():
 
     etiquetaUsuario = Label(frameRegistro, text = 'Nombre de Usuario')
     etiquetaUsuario.grid(row = 0, column = 0, sticky = 'w')
+    etiquetaUsuario.config(bg='black')
+    etiquetaUsuario.config(fg='white')
     textBoxUsuario = Entry(frameRegistro, textvariable = usuarioRegistro)
     textBoxUsuario.grid(row = 0, column = 1, sticky = 'w')
+    textBoxUsuario.config(bg = 'black')
+    textBoxUsuario.config(fg='white')
 
     etiquetaContrasena = Label(frameRegistro, text = 'Contraseña')
     etiquetaContrasena.grid(row = 1, column = 0, sticky = 'w')
+    etiquetaContrasena.config(bg='black')
+    etiquetaContrasena.config(fg='white')
     textBoxContrasena = Entry(frameRegistro, textvariable = contrasenaRegistro)
     textBoxContrasena.grid(row = 1, column = 1, sticky = 'w')
     textBoxContrasena.config(show = '*')
+    textBoxContrasena.config(bg='black')
+    textBoxContrasena.config(fg='white')
 
     etiquetaContrasenaReingreso = Label(frameRegistro, text = 'Reingrese Contraseña')
     etiquetaContrasenaReingreso.grid(row = 2, column = 0, sticky = 'w')
+    etiquetaContrasenaReingreso.config(bg='black')
+    etiquetaContrasenaReingreso.config(fg='white')
     textBoxContrasenaReingreso = Entry(frameRegistro, textvariable = contrasenaReingreso)
     textBoxContrasenaReingreso.grid(row = 2, column = 1, sticky = 'w')
     textBoxContrasenaReingreso.config(show = '*')
+    textBoxContrasenaReingreso.config(bg='black')
+    textBoxContrasenaReingreso.config(fg='white')
 
-    botonRegistro = Button(frameRegistro, text = "Aceptar", command = lambda : aplicar_register(usuarioRegistro.get(), contraseñaRegistro.get(), contraseñaReingreso.get(), rootRegistro, 'usuarios.csv'))
-    botonRegistro.grid(row = 3, column = 1, sticky = 'w')
+    botonRegistro = Button(rootRegistro,
+                           text = "Enter",
+                           command = lambda : aplicar_register(usuarioRegistro.get(),
+                                                               contrasenaRegistro.get(),
+                                                               contrasenaReingreso.get(),
+                                                               rootRegistro, 'archivos/usuarios.csv'))
+    botonRegistro.grid(row = 5, column = 2)
 
 
 def gui():
@@ -197,11 +244,13 @@ def gui():
     la intefaz grafica Main del modulo usada para el login de cada usuario, y trabaja en conjunto,
     con la otra gui (la de registro).
     #Maximo Utrera
+    los botones de registro y exit.
+    #Florencia russo
     """
     #Root of GUI
     root = Tk()
-    root.iconbitmap('archivos/icono.ico')
-    root.title('Wordle G1 Log-in')
+    root.iconbitmap('archivos/pikachu.ico')
+    root.title('Wordle G1 | Log-in')
     root.resizable(False, False)
     root.geometry('350x200')
     root.config(bg= 'hot pink')
@@ -211,7 +260,7 @@ def gui():
     frame.config(bg= 'black')
     frame.config(relief='groove')
     frame.config(bd=5)
-    frame.grid(rowspan = 5, columnspan = 5, padx=60, pady=20)
+    frame.grid(rowspan = 5, columnspan = 5, padx=70, pady=20)
 
     #All the labels below
     LabelUser = Label(frame, text='Username:')
@@ -247,7 +296,7 @@ def gui():
 
     ButtonOut = Button(root, text='Exit login',
                          command = lambda: root.destroy())
-    ButtonOut.grid(row = 6, column= 4, padx= 5, pady= 5)
+    ButtonOut.grid(row = 6, column= 2, padx= 5, pady= 5)
     
 
     root.mainloop()
